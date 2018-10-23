75330c09679ed32148f83a46b82ab1d7d69210df
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-10-23 14:16:23 -0600

    fixed localhost issue

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.use((_req, res) => {
     res.sendStatus(404);
